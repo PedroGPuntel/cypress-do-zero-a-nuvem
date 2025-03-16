c0d42f9b894b46edeb14ef21caca5548667a46c3
Pedro Guedes Puntel <pgpuntel@gmail.com>
2025-03-16 20:22:01 -0300

    configs para Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},  
+  e2e: {}, 
+  module.exports = {
+    projectId: "furp8m",
+  }
 })
